980bce572c8ed930971708125d61964cb0a76cff
GouravRusiya30 <gouravrusiya.lnct@gmail.com>
2020-07-15 00:34:20 +0530

    re-formatted

--- welcome-to-java.java
+++
@@ -1,8 +0,0 @@
-public class Solution {
-
-    public static void main(String[] args) {
-        /* Enter your code here. Print output to STDOUT. Your class should be named Solution. */
-        System.out.println("Hello, World.");
-        System.out.println("Hello, Java.");
-    }
-}
