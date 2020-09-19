93385874a745ba700da93c5d63f7f790c4d92618
Gourav Rusiya <gouravrusiya.lnct@gmail.com>
2020-09-19 09:38:17 +0530

    Create java-output-formatting.java

---
+++ Java/java-output-formatting.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[] args) {
+            Scanner sc=new Scanner(System.in);
+            System.out.println("================================");
+            for(int i=0;i<3;i++)
+            {
+                String s1=sc.next();
+                int x=sc.nextInt();
+                //Complete this line
+                System.out.printf("%-15s%03d",s1,x);
+                System.out.println();
+            }
+            System.out.println("================================");
+
+    }
+}
